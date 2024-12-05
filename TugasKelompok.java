86122b9515237d24037a6547427478778f9af012
fajareka1 <fajareka.haltec@gmail.com>
2024-12-05 16:00:43 +0700

    a

--- TugasKelompok.java
+++ TugasKelompok.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 public class TugasKelompok {
     static int[][] dataMahasiswa = new int[100][100];
-    static int[] arrsks = new int[100];
-    static String[] nama = new String[100];
-    static String[] arrkode = new String[100];
-    static String[] arrmatkul = new String[100];
     static int[] nim = new int[100];
-    
+    static int[] totalSks = new int[100];
+    static String[] nama = new String[100];
+    static String[][] arrkode = new String[100][100];
+    static String[][] arrmatkul = new String[100][100];
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -22,7 +21,10 @@ public class TugasKelompok {
                 index++;
             }else if(pilihan == 2){
                 tampilData();
+            }else if(pilihan == 3){
+                analisisData();
             }else if(pilihan == 4){
+                System.out.println("Terimakasih !!!!");
                 break;
             }
         }
@@ -30,7 +32,7 @@ public class TugasKelompok {
     }
 
     static void menu(){
-        System.out.println("=== Sistem pemantauan KRS Mahasiswa");
+        System.out.println("\n=== Sistem pemantauan KRS Mahasiswa");
         System.out.println("1. Tambah Data KRS");
         System.out.println("2. Tampilkan Daftar KRS Mahasiswa");
         System.out.println("3. Analisis Data KRS");
@@ -48,9 +50,9 @@ public class TugasKelompok {
         String inpnama = input.nextLine();
         System.out.print("NIM : ");
         int inpnim = input2.nextInt();
+        nim[index] = inpnim;
+        nama[index] = inpnama;
         while (true) {
-            nim[jmlSks] = inpnim;
-            nama[jmlSks] = inpnama;
             System.out.print("Kode Mata Kuliah : ");
             String kode = input.nextLine();
             System.out.print("Nama Mata Kuliah : ");
@@ -69,34 +71,38 @@ public class TugasKelompok {
             System.out.println("Data mata kuliah berhasil ditambahkan. ");
             System.out.print("Tambah mata kuliah lain (y/n) : ");
             lanjut = input.nextLine();
+            arrkode[index][jmlSks] = kode;
+            arrmatkul[index][jmlSks] = matkul;
+            dataMahasiswa[index][jmlSks] = sks;
+            jmlSks++;
             if(lanjut.equalsIgnoreCase("n")){
                 System.out.println("Total SKS yang diambil: " + sksTotal);
                 break;
             }
-            arrkode[jmlSks] = kode;
-            arrmatkul[jmlSks] = matkul;
-            dataMahasiswa[index][jmlSks] = sks;
-            jmlSks++;
-           
         }
+        totalSks[index] = sksTotal;
     }
+
+
 
     static void tampilData(){
         int index = 0, search;
         Scanner input = new Scanner(System.in);
         System.out.print("NIM : ");
         search = input.nextInt();
+        System.out.println("\nDaftar KRS : ");
 
         for(int a = 0; a < nim.length; a++){
             if(nim[a] == search){
-                for(int i = 0; i < dataMahasiswa[a].length; i++){
-                    if(dataMahasiswa[a][i] == 0){
-                        continue;
-                    }else{
-                        System.out.print(nim[a] + "\t" + nama[a] + "\t" + arrkode[a]  + "\t" + arrmatkul[a] + "\t" +  dataMahasiswa[a][i]);
-                        System.out.println();
-                    }
-                }
+                index = a;
+            }
+        }
+
+        System.out.println("NIM\tNAMA\tKODE\tMATKUL\tSKS");
+        for(int i = 0; i < dataMahasiswa[0].length; i++){
+            if(dataMahasiswa[index][i] != 0){
+                System.out.print(nim[index] + "\t" + nama[index] + "\t" + arrkode[index][i]  + "\t" + arrmatkul[index][i] + "\t" +  dataMahasiswa[index][i]);
+                System.out.println();
             }
         }
 
@@ -104,7 +110,14 @@ public class TugasKelompok {
     }
 
 
-    static void analisisData(Scanner input){
+    static void analisisData(){
+        int banyak = 0;
+        for(int i = 0; i < totalSks.length; i++){
+            if(totalSks[i] < 20 && totalSks[i] != 0){
+                banyak += 1;
+            }
+        }
 
+        System.out.println("Banyak Mahasiswa yang Memiliki total SKS kurang dari 20 : " + banyak);
     }
 }
